5e0cea955e3ed47cba223b77f41195e487296ef4
Ankush Yadav <129288727+yadavankush2404@users.noreply.github.com>
2025-09-15 16:01:08 +0530

    Add files via upload

---
+++ a.py
@@ -0,0 +1,21 @@
+def first(arr,x,n):
+    low =0
+    high=n-1
+    res=-1
+    while(low<=high):
+        mid = (low+high)//2
+        if arr[mid]==x:
+            res = mid
+            high = mid-1
+        elif arr[mid]<x:
+            low = mid+1
+        elif arr[mid] >x:
+            high =mid -1;
+    return res;
+
+# Driver code
+arr = [1, 2, 2, 2, 2, 3, 4, 7, 8, 8]
+n = len(arr)
+x = 2
+ 
+print("First Occurrence =", first(arr, x, n))
